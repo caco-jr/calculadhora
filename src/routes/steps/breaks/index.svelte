055a8e75358d46c8c0edc039b4571e06f282a528
Carlos Costa <jr.carlosad@gmail.com>
2020-06-11 20:43:08 -0300

    feat: Navegação entre as páginas

--- src/routes/steps/breaks/index.svelte
+++ src/routes/steps/breaks/index.svelte
@@ -1,6 +1,8 @@
 <script>
   import LayoutBase from "../components/LayoutBase";
   import ProgressBar from "../components/ProgressBar";
+  import Button from "@components/Button";
+  import { STEP_RESULT_URI } from "@lib/routesURI";
 </script>
 
 <svelte:head>
@@ -9,5 +11,8 @@
 
 <LayoutBase title="Pausas:">
   <section>Apagar</section>
+
+  <Button tag="a" href={STEP_RESULT_URI}>Próximo</Button>
+
   <ProgressBar currentStepNumber="2" />
 </LayoutBase>
--- src/routes/steps/lunch/index.svelte
+++ src/routes/steps/lunch/index.svelte
@@ -1,6 +1,8 @@
 <script>
   import LayoutBase from "../components/LayoutBase";
   import ProgressBar from "../components/ProgressBar";
+  import Button from "@components/Button";
+  import { STEP_BREAK_URI } from "@lib/routesURI";
 </script>
 
 <svelte:head>
@@ -9,5 +11,8 @@
 
 <LayoutBase title="Almoço:">
   <section>Hora do almoço</section>
+
+  <Button tag="a" href={STEP_BREAK_URI}>Próximo</Button>
+
   <ProgressBar currentStepNumber="1" />
 </LayoutBase>
